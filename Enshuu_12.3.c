3b4780985fd67b21791f6bfc4f1dc19076afe3a3
BiibunSekibun_jp <ia7903955@gmail.com>
2022-06-11 22:13:13 +0900

    変更

--- Enshuu_12.3.c
+++ Enshuu_12.3.c
@@ -14,6 +14,7 @@ int main(void)
 	int a[NROW][NCOL] = { {1,2,3},{4,5,6},{7,8,9} };
 	int b[NROW][NCOL] = { {11,12,13},{14,15,16},{17,18,19} };
 	int c[NROW][NCOL];
+	printf("å≥ÇÃÇQÇ¬ÇÃçsóÒ");
 	display(a);
 	display(b);
 	TASHIZAN(a,b, c);
